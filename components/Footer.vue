3f60a0ae331ce6450db51c81f017fd748b2e919a
aymaneMx <aymanemx@gmail.com>
2022-01-01 19:57:27 +0100

    :foot: update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,7 @@
     <div class="md:py-5">
       <div class="flex flex-col items-center justify-between mt-6 md:mt-0 md:flex-row">
         <div class="logo flex items-center">
-          <p class="text-sm text-gray-700 dark:text-gray-200 mx-1">© 2021 {{ $config.githubUsername }} Built with ♡ </p>
+          <p class="text-sm text-gray-700 dark:text-gray-200 mx-1">© 2022 {{ $config.githubUsername }} Built with ♡ </p>
           <a
 aria-label="AymaneMx.com" class="text-sm underline text-gray-700 dark:text-gray-200"
              href="https://github.com/aymaneMx/nuxt-portfolio-dev">Open sourced on Github</a>
