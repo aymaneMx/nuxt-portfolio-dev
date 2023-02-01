c9963053e5ac32f3298bc021e56d59d8d1a8c1b9
Nikki <nikkiweat@gmail.com>
2023-02-01 14:48:34 +0800

    setup for test deployment via github actions

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/nweat.github.io/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: process.env.GITHUB_USERNAME,
