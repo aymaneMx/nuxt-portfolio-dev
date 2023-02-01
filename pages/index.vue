a841a53f02188d30e83ee5dc65303acff72243e3
Nikki <nikkiweat@gmail.com>
2023-02-01 14:11:14 +0800

    update favicon and github api search query

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,13 @@ export default {
   async asyncData({ $axios, $notion, $config: { githubUsername, notionTableId } }) {
     const projects = await $axios
       .get(
-        'https://api.github.com/search/repositories?q=user:' + githubUsername + '&sort=stars&per_page=3'
+        'https://api.github.com/search/repositories?q=user:' + githubUsername + '+portfolio+:in:description&sort=updated&per_page=3'
       )
       .catch((errors) => {
         // console.log(errors)
       })
     const pageTable = await $notion.getPageTable(notionTableId)
-    const posts = pageTable.filter((page) => page.public).sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    const posts = pageTable.filter((page) => page.public).sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
     return { posts, projects: projects.data.items }
   }
 }
